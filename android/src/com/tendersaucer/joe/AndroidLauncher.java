cc8fe7f013576b08b6723bdf5b35882b93efc6e9
Alex Schimpf <aschimpf1@gmail.com>
2016-09-04 23:53:18 -0700

    cleaning up levels

--- android/src/com/tendersaucer/joe/AndroidLauncher.java
+++ android/src/com/tendersaucer/joe/AndroidLauncher.java
@@ -9,6 +9,7 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Game(), config);
 	}
